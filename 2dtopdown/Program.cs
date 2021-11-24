2e51807a65eed71d64c55d7f8a98a615cdab96d5
Olle Järvheden <jarvheden.olle@elev.ga.ntig.se>
2021-11-24 11:08:25 +0100

    voolt

---
+++ 2dtopdown/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Raylib_cs;
+
+Raylib.InitWindow(800,600, "2D-topdown");
+Raylib.SetTargetFPS(60);
+
+
+Rectangle playerRect = new Rectangle(10, 10, 80, 80);
+
+
+
+while (!Raylib.WindowShouldClose())
+{
+
+
+
+Raylib.BeginDrawing();
+
+Raylib.ClearBackground(Color.PURPLE);
+
+Raylib.DrawRectangleRec(playerRect, Color.BLACK);
+
+Raylib.EndDrawing();
+
+}
+
+
+
+
