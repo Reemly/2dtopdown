f2bc5883e3a0adcd98255eeddca22b94eb9264e1
Olle Järvheden <jarvheden.olle@elev.ga.ntig.se>
2021-11-30 11:03:05 +0100

    jhbghjgyuj

--- 2dtopdown/Program.cs
+++ 2dtopdown/Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using Raylib_cs;
 
-Raylib.InitWindow(800,600, "2D-topdown");
+Raylib.InitWindow(800, 600, "2D-topdown");
 Raylib.SetTargetFPS(60);
 
-
+Texture2D playerImage = Raylib.LoadTexture("kool.png");
 Rectangle playerRect = new Rectangle(10, 10, 80, 80);
 
 
@@ -13,14 +13,38 @@ while (!Raylib.WindowShouldClose())
 {
 
 
+    Raylib.BeginDrawing();
 
-Raylib.BeginDrawing();
 
-Raylib.ClearBackground(Color.PURPLE);
 
-Raylib.DrawRectangleRec(playerRect, Color.BLACK);
+    Raylib.ClearBackground(Color.PURPLE);
 
-Raylib.EndDrawing();
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
+    {
+        playerRect.x -= 3;
+    }
+
+if (Raylib.IsKeyDown(KeyboardKey.KEY_S))
+    {
+        playerRect.y += 3;
+    }
+
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
+    {
+        playerRect.x += 3;
+    }
+
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
+    {
+        playerRect.y -= 3;
+        //Raylib.DrawTriangle (10, 10, 10, );
+    }
+
+    Raylib.DrawRectangleRec(playerRect, Color.WHITE);
+
+    Raylib.DrawTexture(playerImage, (int)playerRect.x, (int)playerRect.y, Color.WHITE);
+
+    Raylib.EndDrawing();
 
 }
 
