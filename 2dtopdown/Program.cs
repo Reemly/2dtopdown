583303b8abeee051e420a2c097c7bdac3308ba60
Olle Järvheden <jarvheden.olle@elev.ga.ntig.se>
2021-12-08 11:07:42 +0100

    oowertjsoitj

--- 2dtopdown/Program.cs
+++ 2dtopdown/Program.cs
@@ -3,21 +3,21 @@ using System;
 using Raylib_cs;
 using System.Numerics;
 
-Raylib.InitWindow(800, 600, "2D-topdown");
+Raylib.InitWindow(800, 800, "2D-topdown");
 Raylib.SetTargetFPS(60);
 
 string room = "Original";
+
+int points = 0;
 
 Texture2D gameImage2 = Raylib.LoadTexture("door.png");
 Texture2D gameImage = Raylib.LoadTexture("kool.png");
 Rectangle gameRect = new Rectangle(10, 10, 80, 80);
 Rectangle gameRect2 = new Rectangle(200, 200, 50, 50);
-
-Vector2 textPos = new Vector2(50, 25);
-
-
 Rectangle r1 = new Rectangle(200, 200, 50, 50);
 
+bool pointRetrieved = false;
+//fixa poäng olle, använd boolen och inten, kolla mickes antäckningar i github etc
 
 while (!Raylib.WindowShouldClose())
 {
@@ -63,27 +63,31 @@ while (!Raylib.WindowShouldClose())
     }
     else
     {
-    Raylib.ClearBackground(Color.ORANGE);
+        Raylib.ClearBackground(Color.ORANGE);
+         
+
+
     }
 
-if (room == "room2")
-{
-Raylib.ClearBackground(Color.VIOLET);
-Raylib.DrawText("You winner!!", 275, 300 , 40, Color.BLACK);
+    if (room == "room2")
+    {
+        Raylib.ClearBackground(Color.VIOLET);
+        Raylib.DrawText("You winner!!", 275, 300, 40, Color.BLACK);
 
-    if (Raylib.CheckCollisionRecs(r1, gameRect))
-    {
-        room = "Original";
+        if (Raylib.CheckCollisionRecs(r1, gameRect))
+        {
+            room = "Original";
+        }
+        else
+        {
+            Raylib.ClearBackground(Color.VIOLET);
+        }
     }
-    else
-    {
-    Raylib.ClearBackground(Color.VIOLET);
-    }
-}
+
 
     Raylib.DrawRectangleRec(gameRect2, Color.WHITE);
 
-    Raylib.DrawTexture(gameImage2, 200, 200, Color.WHITE); 
+    Raylib.DrawTexture(gameImage2, 200, 200, Color.WHITE);
 
     Raylib.DrawRectangleRec(gameRect, Color.WHITE);
 
