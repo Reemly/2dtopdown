3cb19ebe5dcdb84ad8d9f44e9fa9e70b0061d5ac
Olle Järvheden <jarvheden.olle@elev.ga.ntig.se>
2022-01-19 11:05:31 +0100

    commmit

--- 2dtopdown/Program.cs
+++ 2dtopdown/Program.cs
@@ -13,8 +13,9 @@ Texture2D doorImage = Raylib.LoadTexture("door.png");
 Texture2D heroImage = Raylib.LoadTexture("hero.png");
 Rectangle gameRect = new Rectangle(10, 10, 80, 80);
 Rectangle doorRect = new Rectangle(750, 275, 50, 50);
-Rectangle r1 = new Rectangle(750, 275, 50, 50);
+Rectangle r1 = new Rectangle(doorRect.x, doorRect.y, 50, 50);
 
+    room = "room1";
 
 while (!Raylib.WindowShouldClose())
 {
@@ -24,6 +25,10 @@ while (!Raylib.WindowShouldClose())
 
 
     // -- GENERAL
+
+        Raylib.DrawRectangleRec(gameRect, Color.WHITE);
+        Raylib.DrawTexture(heroImage, (int)gameRect.x, (int)gameRect.y, Color.WHITE);
+
 
     if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
     {
@@ -51,48 +56,52 @@ while (!Raylib.WindowShouldClose())
     if (room == "room1")
     {
         Raylib.ClearBackground(Color.ORANGE);
-        
+//Door/room1
+        Raylib.DrawRectangleRec(doorRect, Color.WHITE);
+        Raylib.DrawTexture(doorImage,
+            Raylib.GetScreenWidth() - doorImage.width,
+            Raylib.GetScreenHeight() / 2 - doorImage.height / 2,
+            Color.WHITE);
+//Hero/room1
+
+//InteractCheck/room1
         if (Raylib.CheckCollisionRecs(r1, gameRect))
         {
-            gameRect.x += 2;
-            room = "room2";
+            if (Raylib.IsKeyPressed(KeyboardKey.KEY_E))
+            {
+                room = "room2";
+            }
 
 
         }
     }
 
-    // ROOM 2s
+    // ROOM 2
 
     if (room == "room2")
     {
         Raylib.ClearBackground(Color.DARKGREEN);
+//Door/room2
+        Raylib.DrawRectangleRec(doorRect, Color.WHITE);
+        Raylib.DrawTexture(doorImage, 0, Raylib.GetScreenHeight() / 2 - doorImage.height / 2, Color.WHITE);
 
+//Hero/room2
+
+//InteractCheck/room1
         if (Raylib.CheckCollisionRecs(r1, gameRect))
         {
-            room = "Original";
-        }
-        else
-        {
-            Raylib.ClearBackground(Color.DARKGREEN);
+            if (Raylib.IsKeyPressed(KeyboardKey.KEY_E))
+            {
+                Raylib.DrawText("Hi",300, 200, 90,Color.BLACK);
+            }
         }
     }
 
-
-    Raylib.DrawRectangleRec(doorRect, Color.WHITE);
-
-    Raylib.DrawTexture(doorImage,
-        Raylib.GetScreenWidth() - doorImage.width,
-        Raylib.GetScreenHeight() / 2 - doorImage.height / 2,
-        Color.WHITE);
-
-    Raylib.DrawRectangleRec(gameRect, Color.WHITE);
-
-    Raylib.DrawTexture(heroImage, (int)gameRect.x, (int)gameRect.y, Color.WHITE);
 
     Raylib.EndDrawing();
 
 }
 
 
-
+//MESSAGE OF LAST LESSON: Du ska göra så att dörren av bilden hamnar tvärt emot där den ligger i rum 1, sen så ska du också göra en ny bild där dörren har gller framför sig, och du ska göra så att om man trycker E på dörren bakom galler så kommer det upp ett meddelande som säger att den är låst.
 
